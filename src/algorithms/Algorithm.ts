80530794c4130c6e9a570cac51da9a5f928f0f93
Kenneth Ezekiel <88850771+KenEzekiel@users.noreply.github.com>
2023-04-20 01:19:48 +0700

    doc: doxygen on Algorithm

--- src/algorithms/Algorithm.ts
+++ src/algorithms/Algorithm.ts
@@ -1,6 +1,11 @@
 // Placeholder for parent algorithm class
 abstract class Algorithm {
 
+    /**
+     *
+     * @param target substring from input
+     * @param question question to be matched against
+     */
     abstract check(target: String, question: String): boolean;
     // Target is the substring, question is the subject to be matched
 }
