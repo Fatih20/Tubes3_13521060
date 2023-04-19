05e3f407c70cea0c63c04002c549162e8df61a5f
KENNETH EZEKIEL <ken35kiel@gmail.com>
2023-04-20 00:26:53 +0700

    feat: initialized algorithm parent class

--- src/algorithms/Algorithm.ts
+++ src/algorithms/Algorithm.ts
@@ -1,1 +1,13 @@
 // Placeholder for parent algorithm class
+abstract class Algorithm {
+    targetString: string;
+    substring: string;
+
+    constructor(targetString: string, substring: string) {
+        this.targetString = targetString;
+        this.substring = substring;
+    }
+
+    abstract check(): boolean;
+
+}
