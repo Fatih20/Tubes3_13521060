552a0e4e3e3c4de0357012a607144a07f6e783be
Fatih20 <fnri39@protonmail.com>
2023-04-24 00:31:10 +0700

    Proper scrolling of session history.

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -5,30 +5,35 @@ import ChatHistory from "./ChatHistory";
 function Sidebar() {
   const { stringMatcher, setBM, setKMP } = useStringMatcherContext();
   return (
-    <div className="flex flex-col items-center justify-start w-fit px-8 py-4">
-      <h1 className="text-3xl font-bold text-center mb-5">Rinum</h1>
+    <div className="flex flex-col items-center justify-start w-fit px-8 py-4 h-full gap-4">
+      <h1 className="text-3xl font-bold text-center">Rinum</h1>
       <ChatHistory />
-      <div className="flex-row flex gap-2 mb-4">
+      <div className="flex flex-col items-center justify-start gap-4 w-full">
+        <div className="flex-row flex gap-2 w-full">
+          <button
+            className={`btn btn-xs w-1/2 ${
+              stringMatcher === "KMP" ? "btn-secondary" : "btn-ghost"
+            }`}
+            onClick={setKMP}
+          >
+            KMP
+          </button>
+          <button
+            className={`btn btn-xs w-1/2 ${
+              stringMatcher === "BM" ? "btn-secondary" : "btn-ghost"
+            }`}
+            onClick={setBM}
+          >
+            BM
+          </button>
+        </div>
         <button
-          className={`btn btn-xs w-1/2 ${
-            stringMatcher === "KMP" ? "btn-secondary" : "btn-ghost"
-          }`}
-          onClick={setKMP}
+          className="btn btn-primary normal-case w-full btn-sm"
+          onClick={() => signOut()}
         >
-          KMP
-        </button>
-        <button
-          className={`btn btn-xs w-1/2 ${
-            stringMatcher === "BM" ? "btn-secondary" : "btn-ghost"
-          }`}
-          onClick={setBM}
-        >
-          BM
+          Sign Out
         </button>
       </div>
-      <button className="btn btn-primary normal-case" onClick={() => signOut()}>
-        Sign Out
-      </button>
     </div>
   );
 }
