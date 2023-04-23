ce2591ba802f03bd9067771aa22010880ffa8a31
Fatih20 <fnri39@protonmail.com>
2023-04-23 20:35:39 +0700

    Conversation box working.

--- src/components/Chatbox.tsx
+++ src/components/Chatbox.tsx
@@ -1,5 +1,6 @@
 import { useStringMatcherContext } from "@/contexts/StringMatcher";
 import { useState } from "react";
+import ConversationBox from "./ConversationBox";
 
 function Chatbox() {
   const { stringMatcher } = useStringMatcherContext();
@@ -7,9 +8,9 @@ function Chatbox() {
   const maxLength = 10000;
 
   return (
-    <div className="flex-col w-full items-center justify-center bg-white py-4 px-4 gap-4 h-full">
-      <div className="flex-grow bg-slate-500"></div>
-      <div className="flex justify-start items-stretch gap-2 bg-secondary rounded-md p-2 h-fit">
+    <div className="flex flex-col w-full items-center justify-center bg-white py-4 px-4 gap-4 h-full">
+      <ConversationBox />
+      <div className="flex justify-start items-stretch gap-2 bg-secondary rounded-md p-2 h-fit w-full">
         <div className="flex flex-col w-full">
           <textarea
             value={prompt}
