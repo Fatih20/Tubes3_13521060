f596b3c7669eaf9d9c3ad1a320c6a4fba4c8a579
Fatih20 <fnri39@protonmail.com>
2023-04-23 20:02:07 +0700

    Add label to textarea.

--- src/components/Chatbox.tsx
+++ src/components/Chatbox.tsx
@@ -3,12 +3,28 @@ import { useState } from "react";
 
 function Chatbox() {
   const { stringMatcher } = useStringMatcherContext();
+  const [prompt, setPrompt] = useState("");
+  const maxLength = 10000;
+
   return (
     <div className="flex-col w-full items-center justify-center bg-white py-4 px-4 gap-4 h-full">
       <div className="flex-grow bg-slate-500"></div>
-      <div className="flex justify-start items-stretch gap-2">
-        <textarea className="w-full textarea textarea-bordered textarea-primary"></textarea>
-        <button className="btn btn-primary h-full self-stretch">Send</button>
+      <div className="flex justify-start items-stretch gap-2 bg-secondary rounded-md p-2 h-fit">
+        <div className="flex flex-col w-full">
+          <textarea
+            value={prompt}
+            onChange={(e) => setPrompt(e.target.value)}
+            className="w-full textarea bg-transparent resize-none border-0 outline-0 focus:outline-none shadow-none h-28"
+          ></textarea>
+          <label
+            className={`self-end label-text ${
+              prompt.length <= maxLength ? "" : "text-error"
+            }`}
+          >
+            {prompt.length}/{maxLength}
+          </label>
+        </div>
+        <button className="btn bg-transparent border-0 text-white">Send</button>
         <div className="flex flex-col items-center justify-center gap-2"></div>
       </div>
     </div>
