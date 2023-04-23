8b12956bed1f331a124828601abeda35b167315c
Fatih20 <fnri39@protonmail.com>
2023-04-23 11:57:45 +0700

    Change input to textarea.

--- src/components/Chatbox.tsx
+++ src/components/Chatbox.tsx
@@ -5,13 +5,10 @@ function Chatbox() {
   return (
     <div className="flex-col w-full items-center justify-center bg-white py-4 px-4 gap-4 h-full">
       <div className="flex-grow bg-slate-500"></div>
-      <div className="flex justify-start items-center gap-2">
-        <input className="input w-full input-bordered input-primary" />
-        <div className="flex flex-col items-center justify-center gap-2">
-          <button className="btn btn-xs w-full">KMP</button>
-          <button className="btn btn-xs w-full">BM</button>
-        </div>
-        <button className="btn btn-primary">Send</button>
+      <div className="flex justify-start items-stretch gap-2">
+        <textarea className="w-full textarea textarea-bordered textarea-primary"></textarea>
+        <button className="btn btn-primary h-full">Send</button>
+        <div className="flex flex-col items-center justify-center gap-2"></div>
       </div>
     </div>
   );
