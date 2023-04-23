5b38aec72148cd7cd3fe0d290f1ac7d9792ad77d
Fatih20 <fnri39@protonmail.com>
2023-04-24 02:01:22 +0700

    Initialize context for which chat session is selected.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import { SessionProvider } from "next-auth/react";
 import { type Session } from "next-auth";
 import StringMatcherProvider from "@/contexts/StringMatcherProvider";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import ChatSessionProvider from "@/contexts/ChatSessionProvider";
 
 const queryClient = new QueryClient();
 
@@ -14,9 +15,11 @@ const App: AppType<{ session: Session | null }> = ({
   return (
     <QueryClientProvider client={queryClient}>
       <SessionProvider session={session}>
-        <StringMatcherProvider>
-          <Component {...pageProps} />
-        </StringMatcherProvider>
+        <ChatSessionProvider>
+          <StringMatcherProvider>
+            <Component {...pageProps} />
+          </StringMatcherProvider>
+        </ChatSessionProvider>
       </SessionProvider>
     </QueryClientProvider>
   );
