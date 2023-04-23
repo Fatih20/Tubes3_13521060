837cdd21be3dbfaa31773360098b04034e88d041
Fatih20 <fnri39@protonmail.com>
2023-04-23 12:31:19 +0700

    Implement KMP-BM selector in the sidebar.

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -7,6 +7,24 @@ function Sidebar() {
     <div className="flex flex-col items-center justify-start w-fit px-8 py-4">
       <h1 className="text-3xl font-bold text-center">Rinum</h1>
       <div className="h-full flex-grow"></div>
+      <div className="flex-row flex gap-2 mb-4">
+        <button
+          className={`btn btn-xs w-1/2 ${
+            stringMatcher === "KMP" ? "btn-secondary" : "btn-ghost"
+          }`}
+          onClick={setKMP}
+        >
+          KMP
+        </button>
+        <button
+          className={`btn btn-xs w-1/2 ${
+            stringMatcher === "BM" ? "btn-secondary" : "btn-ghost"
+          }`}
+          onClick={setBM}
+        >
+          BM
+        </button>
+      </div>
       <button className="btn btn-primary normal-case" onClick={() => signOut()}>
         Sign Out
       </button>
