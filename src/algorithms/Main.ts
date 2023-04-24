6b8fab107ad7c20b7ffcb532cc1fd8dea3aec416
Fatih20 <fnri39@protonmail.com>
2023-04-24 23:44:32 +0700

    Minor tidying up of main.

--- src/algorithms/Main.ts
+++ src/algorithms/Main.ts
@@ -1,59 +1,67 @@
-import { PrioQueue } from "./PrioQueue"
-import { Algorithm } from "./Algorithm"
-import { KMP } from "./KMP"
-import { BM } from "./BM"
-import { DistanceAlgorithm } from "./DistanceAlgorithm"
+import { PrioQueue } from "./PrioQueue";
+import { Algorithm } from "./Algorithm";
+import { KMP } from "./KMP";
+import { BM } from "./BM";
+import { DistanceAlgorithm } from "./DistanceAlgorithm";
 
 class Main {
-    ListQuestion: string[]
-    KMPAlgorithm: Algorithm = new KMP()
-    BMAlgorithm: Algorithm = new BM()
-    DistanceAlgorithm: DistanceAlgorithm = new DistanceAlgorithm()
+  ListQuestion: string[];
+  KMPAlgorithm: Algorithm = new KMP();
+  BMAlgorithm: Algorithm = new BM();
+  DistanceAlgorithm: DistanceAlgorithm = new DistanceAlgorithm();
 
-    constructor(ListQuestion: string[]) {
-        this.ListQuestion = ListQuestion
+  constructor(ListQuestion: string[]) {
+    this.ListQuestion = ListQuestion;
+  }
+
+  /**
+   *
+   * @param subtring The string to check (from input)
+   * @param flag (on : KMP, off : BM)
+   */
+  getMatchingQuestion(substring: string, flag: boolean): string[] {
+    // Iterate over all the questions in the list
+    // Return the first matching question if KMP/BM return true
+    let retval: string[] = [""];
+    let Questions: PrioQueue = new PrioQueue();
+
+    this.ListQuestion.forEach((element) => {
+      let exactMatchExist = flag
+        ? this.KMPAlgorithm.check(substring, element)
+        : this.BMAlgorithm.check(substring, element);
+      if (exactMatchExist) {
+        return [element];
+      } else {
+        // Get question match percentage
+        let matchPercent = this.DistanceAlgorithm.getDistance(
+          substring,
+          element
+        );
+        Questions.enqueue(matchPercent, element);
+      }
+    });
+
+    // Get first element, if > 90% then return
+
+    let elmt = Questions.dequeue();
+    if (elmt != undefined && elmt[0] > 0.9) {
+      retval = [elmt[1]];
+      return [elmt[1]];
+    } else if (elmt != undefined) {
+      retval = [
+        elmt[1],
+        Questions.queue[0][1] == undefined ? "" : Questions.queue[0][1],
+        Questions.queue[1][1] == undefined ? "" : Questions.queue[1][1],
+      ];
+      return [
+        elmt[1],
+        Questions.queue[0][1] == undefined ? "" : Questions.queue[0][1],
+        Questions.queue[1][1] == undefined ? "" : Questions.queue[1][1],
+      ];
     }
 
-    /**
-     *
-     * @param subtring The string to check (from input)
-     * @param flag (on : KMP, off : BM)
-     */
-    getMatchingQuestion(substring: string, flag: boolean): string[] {
-        // Iterate over all the questions in the list
-        // Return the first matching question if KMP/BM return true
-        let retval: string[] = [""]
-        let Questions: PrioQueue = new PrioQueue();
-
-        this.ListQuestion.forEach(element => {
-            let exactMatchExist = false;
-            if (flag) { exactMatchExist = this.KMPAlgorithm.check(substring, element); }
-            else { exactMatchExist = this.BMAlgorithm.check(substring, element);}
-            if (exactMatchExist) { retval = [element]; return [element]; }
-            else {
-                // Get question match percentage
-                let matchPercent = this.DistanceAlgorithm.getDistance(substring, element);
-                Questions.enqueue(matchPercent, element);
-            }
-        });
-
-
-        // Get first element, if > 90% then return
-
-        let elmt = Questions.dequeue()
-        if (elmt != undefined && elmt[0] > 0.9) {
-            retval = [elmt[1]]
-            return [elmt[1]]
-        } else if (elmt != undefined) {
-            retval = [elmt[1], Questions.queue[0][1] == undefined? "": Questions.queue[0][1], Questions.queue[1][1] == undefined? "": Questions.queue[1][1]]
-            return [elmt[1], Questions.queue[0][1] == undefined? "": Questions.queue[0][1], Questions.queue[1][1] == undefined? "": Questions.queue[1][1]]
-        }
-
-        return retval
-
-    }
-
-
+    return retval;
+  }
 }
 
-export { Main }
+export { Main };
