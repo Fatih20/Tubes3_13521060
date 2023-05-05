ebb1f5f9e80392dc62c8e0f53b374e9b2eae751d
MuhamadAjiW <16521119@mahasiswa.itb.ac.id>
2023-05-05 13:47:10 +0700

    fix: adjusted exact algorithm

--- src/algorithms/Main.ts
+++ src/algorithms/Main.ts
@@ -26,24 +26,36 @@ class Main {
     // Return the first matching question if KMP/BM return true
     let Questions: PrioQueue = new PrioQueue();
 
+    let exactMatchExist;
     if(exact){
       for (const element of this.ListQuestion) {
-        let exactMatchExist = algoType
+        exactMatchExist = algoType
           ? this.KMPAlgorithm.check(substring, element.question)
           : this.BMAlgorithm.check(substring, element.question);
-        if (exactMatchExist) {
+        if (exactMatchExist && substring.length == element.question.length) {
           return [element];
         } 
       }
     }
-
+    
+    
+    
+    let exactFound = false;
     for (const element of this.ListQuestion) {
       let exactMatchExist = algoType
         ? this.KMPAlgorithm.check(substring, element.question)
         : this.BMAlgorithm.check(substring, element.question);
       if (exactMatchExist) {
-        return [element];
-      } else {
+        Questions.enqueue(Math.abs(substring.length - element.question.length), element);
+        exactFound = true;
+      }
+      if (exactFound){
+        for(let i = 0; i < Questions.queue.length; i++){
+          console.log(Questions.queue[i][1].question);
+        }
+        return [Questions.queue[0][1]];
+      }
+      else {
         // Get question match percentage
         let matchPercent = this.DistanceAlgorithm.getDistance(
           substring,
